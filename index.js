61f6af5e52907da1d501101b9d319000ce0a65e8
Big Sad <bigsad@Bigs-MBP.vc.shawcable.net>
2020-08-02 16:34:31 -0700

    stripe

--- index.js
+++ index.js
@@ -297,7 +297,7 @@ app.get('/confirm_order', (req,res) => {
 // });
 
  app.get('/pay_now', async (req,res) => {
-   res.redirect("/views/pages/checkout.html");
+   res.redirect("/pages/checkout.html");
  });
 
 var calculateOrderAmount = items => {
