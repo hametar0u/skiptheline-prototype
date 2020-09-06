dbaa7f8ecad50ab888d0bac2f0b64788263c1d98
Big Sad <bigsad@Bigs-MacBook-Pro.local>
2020-09-06 15:25:04 -0700

    a

--- index.js
+++ index.js
@@ -355,9 +355,9 @@ app.get('/confirm_order', async (req,res) => {
 //   delete req.session.cart;
 // });
 
- app.get('/pay_now', async (req,res) => {
-   res.render("pages/checkout.ejs");
- });
+//  app.get('/pay_now', async (req,res) => {
+//    res.render("pages/checkout.ejs");
+//  });
 
 var calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
@@ -367,21 +367,21 @@ var calculateOrderAmount = items => {
 };
 
 //just noticed that by sending the query upon pressing the pay now button, if the client doens't complete the payment transaction, the order still gets logged into the database
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd"
-  });
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { items } = req.body;
+//   // Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: "usd"
+//   });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret
-  });
-});
+//   res.send({
+//     clientSecret: paymentIntent.client_secret
+//   });
+// });
 
-app.listen(4242, () => console.log('Node server listening on port 4242!'));
-
+// app.listen(4242, () => console.log('Node server listening on port 4242!'));
+0
 
 app.get('/pending_orders', checkAuth, function (req, res) {
   res.render('pages/pending_orders.ejs');
