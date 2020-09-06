6d8282c4873533afebced56dce65614253ef85dc
Big Sad <bigsad@Bigs-MacBook-Pro.local>
2020-09-06 16:33:04 -0700

    stuff

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 const nodemailer = require("nodemailer");
 const sgTransport = require('nodemailer-sendgrid-transport');
 const { isNullOrUndefined } = require('util');
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 
